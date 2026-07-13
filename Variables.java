d159ab739f7e7f1e13f3d13535127c5e1ea37e41
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-13 23:13:53 +0530

    Variables in java

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String args[]){
+        //Variable - it is like a container in java which store value in it.
+        int a = 25;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+        int diff = b-a;
+        System.out.println(diff);
+    }
+}
