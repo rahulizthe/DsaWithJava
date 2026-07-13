cc5004b872e34c29b657018abc6dc3dca777f00a
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-13 21:37:34 +0530

    another basic day

---
+++ Demo.java
@@ -0,0 +1,10 @@
+//Here we are understanding the moral of printing the other code in next line.
+
+public class Demo {
+    public static void main(String args[]){
+        System.out.println("hello guys this is rahul");//The "println" function command the next out to print in next line.
+        System.out.print("hello guys this is rahul\nlets code together.");//here "\n" does the same as "println".
+        System.out.print("hello guys this is rahul");//here "print" doesn't follow the other line.
+    }
+    
+}
