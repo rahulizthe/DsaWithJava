49964c3260d72d3bc064a12ac7eac7e7fb992d13
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-14 21:55:03 +0530

    Learning input in java

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Input {
+    public static void main(String args[]){
+        //input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.next();
+        System.out.println("your name is "+name);
+
+
+    }
+    
+}
