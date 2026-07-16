ffd3e3d3511bd4b703506f8b2a143b286e5552ea
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-16 20:40:46 +0530

    use of new input methods

--- Input.java
+++ Input.java
@@ -6,7 +6,13 @@ public class Input {
         System.out.println("Enter your name: ");
         String name = sc.next();
         System.out.println("your name is "+name);
-
+        System.out.println("enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("your age is: "+age);
+        System.out.println("enter your amount: ");
+        float amount =sc.nextFloat();
+        System.out.println("your amount is: "+amount);
+        System.out.println("Hi,"+name+" you are "+age+" old.\nand your amount will be of "+amount+"rupees.\nThankyou "+name);
 
     }
     
