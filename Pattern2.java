730e8d37484e7c9e9f633af11833e61f57472eec
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-20 20:26:53 +0530

    Pattern 4- Inverted half pyramid

--- Pattern2.java
+++ Pattern2.java
@@ -1,4 +1,4 @@
-//Hollow rectangle Pattern in java.
+//Hollow rectangle Pattern with java.
 public class Pattern2 {
     public static void main(String args[]){
         int n=4;
--- Pattern3.java
+++ Pattern3.java
@@ -1,4 +1,4 @@
-//Half pyramid pattern in java
+//Half pyramid pattern with java
 public class Pattern3 {
     public static void main(String args[]){
         int n=4;
