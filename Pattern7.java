a66119430c278a553525b65fa95835ee06852fe7
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-21 21:18:59 +0530

    Pattern-7 Inverted Half pyramid with numbers

--- Pattern7.java
+++ Pattern7.java
@@ -1,9 +1,9 @@
-//half pyramid pattern(numbers) with java.
-public class Pattern6 {
+//Inverted half pyramid pattern(numbers) with java.
+public class Pattern7 {
     public static void main(String args[]){
         int n=5;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print(j+" ");
             }
             System.out.println();
