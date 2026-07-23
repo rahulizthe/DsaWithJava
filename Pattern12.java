86fa02753a697cc85c32c50e14c206e788134639
Rahulizthe <rahulrawat241371@gmail.com>
2026-07-23 21:35:37 +0530

    Hollow Solid rhombus Pattern

---
+++ Pattern12.java
@@ -0,0 +1,25 @@
+//Hollow solid rhombus
+public class Pattern12 {
+    public static void main(String args[]) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print hollow rhombus
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
