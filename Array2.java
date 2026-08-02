2e9fdc3e912c1c42a77819d9314552827f8a9b8d
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-02 18:16:03 +0530

    Made I/O representation

--- Array2.java
+++ Array2.java
@@ -5,8 +5,16 @@ public class Array2{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of an array: ");
         int size = sc.nextInt();
+        
         int number[] = new int[size];
+        //input 
+        for(int i=0;i<size;i++){
+            System.out.print("enter the value of index" + i + ":");
+            number[i] = sc.nextInt();
+            
+        }
         //Here Java automatically print the null value if there is no value initiated.
+        //output
         for(int i=0;i<size;i++){
             System.out.println(number[i]);
         }
