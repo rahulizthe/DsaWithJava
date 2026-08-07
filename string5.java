91fd0c21ca6c26e1fcf011d354ee4b8a40cd65d1
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-07 22:51:55 +0530

    initial commit

---
+++ string5.java
@@ -0,0 +1,17 @@
+public class string5 {
+    public static void main(String[] args) {
+        // 1. Create a string using a literal
+        String greeting = "Hello";
+        String name = "World";
+
+        // 2. Concatenate strings using the + operator
+        String message = greeting + " " + name + "!";
+
+        // 3. Get the length of the string
+        int length = message.length();
+
+        // 4. Print the results
+        System.out.println("Message: " + message);
+        System.out.println("Length: " + length);
+    }
+}   
