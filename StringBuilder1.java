6b704b98f7b54d87981ef8899f7af869469a24e6
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-08 22:12:05 +0530

    StringBuilder intialization

---
+++ StringBuilder1.java
@@ -0,0 +1,13 @@
+public class StringBuilder1 {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("tony");
+        System.out.println(sb);
+
+        //method 1
+        System.out.println(sb.charAt(0));
+
+        //method 2 - replacing the index value
+        sb.setCharAt(0, 'p');
+        System.out.println(sb);
+    }
+}
