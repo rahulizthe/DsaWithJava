1b11f87f9d43826a5f4e67106f9ec518b2a9c9ac
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-12 21:47:14 +0530

    Learning about operators

---
+++ Operators1.java
@@ -0,0 +1,30 @@
+public class Operators1 {
+    //here we are seeing the operators in java.
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        //here are the binary--Arithematic operators
+        //1
+        int add =  a + b ;
+        System.out.println(add);
+        //2
+        int sub = a-b;
+        System.out.println(sub);
+        //3
+        int mul = a*b;
+        System.out.println(mul);
+        //4
+        int div = a/b;
+        System.out.println(div);
+        //5
+        int mod = a%b;
+        System.out.println(mod);
+
+        //Here are the Unary ones
+        //1
+        System.out.println(a++);//means a+1
+        //2 
+        System.out.println(b--);//means b-1
+
+    }
+}
--- StringBuilder2.java
+++ StringBuilder2.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 public class StringBuilder2 {
    public static void main(String args[]) {
      StringBuilder sb = new StringBuilder("HelloWorld");
