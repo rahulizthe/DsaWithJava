be3928d82bbe15686848e4271500f6cb362c4139
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-13 18:40:49 +0530

    Logical Operators

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        //Here we are seeing -- Logical operators 
+        //1
+        if( a ==10 && b==5){
+            System.out.println("good");
+        }
+        //2
+        if(a==10 || b==5){
+            System.out.println("good");
+        }
+        //3
+        if( a!=5 && b!=10){
+            System.out.println("good");
+        }
+    }
+}
