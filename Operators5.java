e662a4ca4f6f1bc7e480a740181dfe673114e558
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-13 19:19:55 +0530

    Assignment Operators

---
+++ Operators5.java
@@ -0,0 +1,18 @@
+public class Operators5 {
+    public static void main(String args[]){
+        //here we are seeing --Assignment Operator
+        //1
+        int a =10;
+        int b =5;
+        //2
+        System.out.println(a+=b);
+        //3
+        System.out.println(b-=a);
+        //4
+        System.out.println(a*=b);
+        //5
+        System.out.println(b/=a);
+    }
+
+    
+}
