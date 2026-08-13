84f7b990b2e603586eeea3849a05fd8131774dc0
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-13 18:57:55 +0530

    Bitwise operator

--- Operators3.java
+++ Operators3.java
@@ -1,4 +1,4 @@
-public class Operators {
+public class Operators3 {
     public static void main(String args[]){
         int a = 10;
         int b = 5;
---
+++ Operators4.java
@@ -0,0 +1,22 @@
+public class Operators4 {
+    public static void main(String args[]) {
+
+        int a = 5;
+        int b = 3;
+        //Here are we seeing --Bitwise Operators
+        //1
+        System.out.println("a & b = " + (a & b));
+        //2
+        System.out.println("a | b = " + (a | b));
+        //3
+        System.out.println("a ^ b = " + (a ^ b));
+        //4
+        System.out.println("~a = " + (~a));
+        //5
+        System.out.println("a << 1 = " + (a << 1));
+        //6
+        System.out.println("a >> 1 = " + (a >> 1));
+        //7
+        System.out.println("a >>> 1 = " + (a >>> 1));
+    }
+}
