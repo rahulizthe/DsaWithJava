a027f0e6ddf378d82b7b307f8ea3771fe6e1b001
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-13 18:35:37 +0530

    Relational operators

---
+++ Operators2.java
@@ -0,0 +1,31 @@
+public class Operators2{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        //Here we are seeing -- Relational operators 
+        //1
+        if( a == b){
+            System.out.println("good");
+        }
+        //2
+        if(a != b){
+            System.out.println("good");
+        }
+        //3
+        if( a > b){
+            System.out.println("good");
+        }
+        //4
+        if( a < b){
+            System.out.println("good");
+        }
+        //5
+        if( a >= b){
+            System.out.println("good");
+        }
+        //6
+        if( a <= b){
+            System.out.println("good");
+        }
+    }
+}
