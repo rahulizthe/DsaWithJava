a7477a4396b40680df408472754515eb28dcdec9
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-20 23:14:41 +0530

    Printing sum of n numbers

---
+++ Recursion3.java
@@ -0,0 +1,14 @@
+public class Recursion3 {
+    public static void PrintSum(int i,int n,int sum){
+        if(i==n){
+            sum+=i;
+            System.out.println(sum);
+            return;
+        }
+        sum+=i;
+        PrintSum(i+1, n, sum);
+    }
+    public static void main(String args[]){
+        PrintSum(1, 5, 0);
+    }
+}
