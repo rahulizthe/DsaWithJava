66bd91cbd14f60f6bca6c419f41ff075a7f8a661
Rahulizthe <rahulrawat241371@gmail.com>
2026-08-20 21:37:54 +0530

    Recursion code new

--- Recursion1.java
+++ Recursion1.java
@@ -1,13 +1,15 @@
 public class Recursion1 {
     //Recursion - Where a function call itself again and again until it reaches the Base-Case.
     public static void PrintNum(int n){
+        if(n==0){
+            return;
+        }
         System.out.println(n);
         PrintNum(n-1);
     }
     public static void main(String args[]){
-        for(int i=5;i>0;i--){
-            System.out.println(i);
-        }
+        int n = 5;
+        PrintNum(n);
     }
     
 }
